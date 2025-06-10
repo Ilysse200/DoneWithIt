d5b2d3ff975d24fd0ef1a917abba857a8a676b68
Ilysse200 <ilysseted500@gmail.com>
2025-06-10 10:50:54 +0200

    Basic Done With It mobile application

--- App.js
+++ App.js
@@ -2,9 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  //simplest way to debug our application during development
+  console.log('App executed!');
+
+  //Chrome debugging
+  let x;
+  x.toString();
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Wanda Guela!</Text>
       <StatusBar style="auto" />
     </View>
   );
